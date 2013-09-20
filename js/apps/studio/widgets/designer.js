c0662748be487f7e9637d19c1de403b31069d992
Ravi Kumar Hamsa <ravi.hamsa@inmobi.com>
2013-09-20 07:25:55 +0530

    update

--- js/apps/studio/widgets/designer.js
+++ js/apps/studio/widgets/designer.js
@@ -1,9 +1,13 @@
 define(['base/view', './designer/elementList', './designer/elementForm', 'text!./designer/designer.html'],function(BaseView, ElementList, ElementForm, designerTemplate){
+
+
+
     var View = BaseView.extend({
         template:designerTemplate,
         actionHandler:function(action, e){
             switch(action){
                 case 'addField':
+                     addField.call(this);
                     break;
             }
             e.actionHandled=true;
@@ -21,6 +25,15 @@ define(['base/view', './designer/elementList', './designer/elementForm', 'text!.
             }
         }
     })
+
+    var idCounter=0;
+    var addField = function(){
+        var _this = this;
+        var list = _this.getSubView('list');
+        var listModel = list.model;
+        var listCollection = listModel.get('items');
+        listCollection.add({id:++idCounter, name:'name'+idCounter});
+    }
 
     return {
         View:View
